16df138d9efe4c4486ec30de5b2800f66596a686
Dandi Taufiqurrahman <dandi.taufiq50@gmail.com>
2026-05-06 09:10:10 +0700

    new code

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,7 +1,7 @@
 class AppConstants {
-  // PENTING: 
+  // PENTING:
   // - Kalau pakai Emulator Android, gunakan: http://10.0.2.2:8080/v1
   // - Kalau pakai HP Asli / iOS Simulator, gunakan IP Laptop kamu (contoh: http://192.168.1.5:8080/v1)
   // - localhost / 127.0.0.1 TIDAK BISA dipakai di Emulator Android
- static const String baseUrl = 'http://10.5.55.17:8080/v1'; 
+  static const String baseUrl = 'http://192.168.100.140:8080/v1';
 }
